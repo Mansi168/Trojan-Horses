9e94f21d179fc7e3b9b679095a13b04d21436a66
Monalika Patnaik <monalikapatnaik114@gmail.com>
2023-01-14 18:50:35 +0530

    changes

--- src/Components/Blogs/BlogElements.js
+++ src/Components/Blogs/BlogElements.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+
 export const BlogContainer=styled.div`
 height:320px;
 display:flex;
@@ -6,6 +7,7 @@ flex-direction:column;
 justify-content:center;
 align-items: center;
 /* background:white; */
+font-family: 'Merriweather';
 background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmvtFeXnOY9_eQyYIlFWIGy4dJwelnIYAIeQ&usqp=CAU);
 background-repeat:no-repeat;
 background-size:cover;
@@ -22,14 +24,16 @@ export const BlogH1=styled.h1`
 color:black;
 margin-bottom:64px; */
 margin-bottom: 64px;
-    font-size: 50px;
+    font-size: 70px;
     line-height: 1.1;
-    font-weight: 600;
+    font-weight: 300;
     text-transform: uppercase;
     color: white;
     text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #0073e6, 0 0 20px #0073e6, 0 0 25px #0073e6, 0 0 30px #0073e6, 0 0 35px #0073e6;
-    /* animation: blinker 1s linear infinite;
-    @keyframes blinker {
+    /* text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #228dff,
+      0 0 70px #228dff, 0 0 80px #228dff, 0 0 100px #228dff, 0 0 150px #228dff; */
+    animation: blinker 1s linear infinite;
+    /* @keyframes blinker {
   50% {
     opacity: 0;
   }
--- src/Components/Blogs/index.js
+++ src/Components/Blogs/index.js
@@ -5,7 +5,7 @@ import { BlogContainer,BlogH1 } from './BlogElements'
 const Blogs=()=>{
    return (
     <BlogContainer id="blogs">
-        <BlogH1>Our Services</BlogH1>
+        <BlogH1>Our Blogs</BlogH1>
         </BlogContainer>
    )
 }
